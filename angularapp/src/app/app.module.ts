654aa766c647785b788a5f3802dad46ce592eb44
Iamneo <techadmin@iamneo.ai>
2023-08-02 11:04:27 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,9 +1,13 @@
+// Import required modules and components
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// Import components
 import { AppComponent } from './app.component';
+
+// Import pipes (assuming 'FilterPipe' is a custom pipe)
 import { FilterPipe } from './filter.pipe';
 
 @NgModule({
